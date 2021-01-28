0c2a0bbaf60e4daeb3c3d66748e461c0f0e20d65
david <daviddecastro42@live.co.uk>
2021-01-28 02:51:36 +0000

    clean app and add header withe logo image

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './App.css';
+import Header from './Header';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className='app'>
+        <Header />
+        {/* header */}
+        {/* headerBlock */}
+      </div>
+    </Router>
+  );
+}
+
+export default App;
